ceacc0af14143ed96ebf45ea6c2cc67163b33e21
Rajasurya <rajasurya061999@gmail.com>
2025-10-03 11:24:15 +0530

    updated routers

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,24 +1,10 @@
 <template>
-  <main>
-    <!-- <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/actors">Actors</RouterLink>
-    <RouterLink to="/movies">Movies</RouterLink>
-    <RouterLink to="/genres">Genres</RouterLink> -->
-    <!-- {{ actors }}
-    {{ movies }}
-    {{ directors }}
-    {{ genres }} -->
-  </main>
+  <main></main>
 </template>
 <script lang="ts">
-import axios from 'axios'
-import NavBar from '@/components/NavBar.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    NavBar,
-  },
+  components: {},
   data() {
     return {
       actors: '',
@@ -27,19 +13,6 @@ export default {
       genres: '',
     }
   },
-  async mounted() {
-    // const actorsResponse = await axios.get('http://localhost:8888/actors')
-    // console.log(actorsResponse)
-    // this.actors = actorsResponse.data.actors
-    // const moviesResponse = await axios.get('http://localhost:8888/movies')
-    // console.log(moviesResponse)
-    // this.movies = moviesResponse.data.movies
-    // const directorsResponse = await axios.get('http://localhost:8888/directors')
-    // console.log(directorsResponse)
-    // this.directors = directorsResponse.data.directors
-    // const genresResponse = await axios.get('http://localhost:8888/genres')
-    // console.log(genresResponse)
-    // this.genres = genresResponse.data.genres
-  },
+  async mounted() {},
 }
 </script>
